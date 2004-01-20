1fcb386303a7129b060e50fe6b433b5e45eeee06
hardkrash <hardkrash@fe636330-a7f5-0310-8408-fdd8893149bb>
2004-01-20 20:07:40 +0000

    added support for already running ssh-agents
    git-svn-id: svn+ssh://tchan@foo-projects.org/var/svn/lunar/moonbase/trunk@5157 fe636330-a7f5-0310-8408-fdd8893149bb

--- kde3-apps/lincvs/start-ssh.sh
+++ kde3-apps/lincvs/start-ssh.sh
@@ -9,4 +9,11 @@
 #
 
 export SSH_ASKPASS=/usr/libexec/ssh-askpass
+
+# The following commands allow the ssh that is called to use a running ssh-agent for keys
+# improvents may be made to this to have a ssh-agent started if one is not running
+export SSH_AGENT_PID=`ps --user $UID -o pid=,comm= | grep ssh-agent | grep -v grep | sed "s/^ //" | cut -d" " -f 1`
+export SSH_AGENT_PPID=`ps --pid=$SSH_AGENT_PID  -o ppid="" | sed "s/ //"`
+export SSH_AUTH_SOCK=`find /tmp/ssh-* -name "agent.$SSH_AGENT_PPID"`
+
 ssh "$@"
